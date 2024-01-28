23a0989767f70bb57de7c8cad01171cd6b526938
Robert R. Russell <robert@rrbrussell.com>
2024-01-27 23:50:08 -0600

    Partition Types and code migrations
    I moved the cylinder head sector address type to its own submodule.
    
    I have about a quarter of a partiontype database imported. It will
    need restructuring later on.

---
+++ src/libmbr/chsa.rs
@@ -0,0 +1,58 @@
+pub mod chsa;
+
+/// A packed CHS structure.
+///
+/// This is in little endian format.
+///
+/// The first byte is the Head address.
+/// The second byte is split between the upper bits of the Cylinder address and
+/// the Sector address.
+/// The third byte is the lower bits of the Cylinder address.
+///
+/// Access and writing methods are provided for you.
+pub struct CylinderHeadSectorAddress {
+    data: [u8; 3],
+    }
+
+impl CylinderHeadSectorAddress {
+    pub fn empty() -> CylinderHeadSectorAddress {
+        return CylinderHeadSectorAddress{ data: [0,0,0] }
+    }
+    
+    /// Get the Cylinder out of a packed CHS structure.
+    pub fn get_cylinder(&self) -> u16 {
+        let high_cylinder: u8 = self.data[1];
+        let low_cylinder: u8 = self.data[2];
+        return ((high_cylinder & CYLINDER_MASK) << 2) as u16
+            + low_cylinder as u16;
+    }
+
+    /// Get the Head out of a packed CHS structure.
+    pub fn get_head(&self) -> u8 {
+        return self.data[0];
+    }
+
+    /// Get the Sector out of a pack CHS structure.
+    pub fn get_sector(&self) -> u8 {
+        return self.data[2] & SECTOR_MASK;
+    }
+
+    /// Set the Cylinder in a packed CHS structure.
+    pub fn set_cylinder(&mut self, cylinder: u16) {
+        let high_cylinder: u8;
+        let low_cylinder: u8 = cylinder as u8;
+        high_cylinder = ((cylinder >> 2) as u8 & CYLINDER_MASK) as u8;
+        self.data[1] = high_cylinder + (self.data[1] & SECTOR_MASK);
+        self.data[2] = low_cylinder;
+    }
+
+    /// Set the Head in a packed CHS structure.
+    pub fn set_head(&mut self, head: u8) {
+        self.data[0] = head;
+    }
+
+    /// Set the Sector in a packed CHS structure.
+    pub fn set_sector(&mut self, sector: u8) {
+        self.data[1] = (sector & CYLINDER_MASK) + (self.data[1] & SECTOR_MASK);
+    }
+}
--- src/libmbr/lib.rs
+++ src/libmbr/lib.rs
@@ -1,9 +1,15 @@
+pub use crate::chsa::CylinderHeadSectorAddress;
+
+#[path = "chsa.rs"]
+pub mod chsa;
 
 /// The MBR for IBM PC and compatibles has a defined size of 512 bytes.
 pub const MBR_SIZE: u16 = 512;
 const CYLINDER_MASK: u8 = 0xc0;
 const SECTOR_MASK: u8 = 0x3f;
 
+/// The different variations of MBR that exist.
+/// Most of these are historical from my research.
 pub enum Variations {
     Classical,
     Modern,
@@ -13,9 +19,30 @@ pub enum Variations {
     Ontrack
 }
 
+/// The bootable or not bootable status
+///
+/// TODO! add extra documentation.
+#[repr(u8)]
+pub enum PartitionStatus {
+    NotBootable = 0x00,
+    Invalid = 0x7F,
+    Bootable = 0x80,
+}
+
+impl From<u8> for PartitionStatus {
+    fn from(value: u8) -> Self {
+        if value == 0 {
+            return PartitionStatus::NotBootable;
+        } else if value == 128 {
+            return PartitionStatus::Bootable;
+        }
+        return PartitionStatus::Invalid;
+    }
+}
+
 /// An partition in the MBR.
 pub struct PartitionEntry {
-    status: u8,
+    status: PartitionStatus,
     chs_first_absolute_sector: CylinderHeadSectorAddress,
     partition_type: u8,
     chs_last_absolute_sector: CylinderHeadSectorAddress,
@@ -23,69 +50,33 @@ pub struct PartitionEntry {
     lba_length: LogicalBlockAddress,
 }
 
-/// A packed CHS structure.
-///
-/// This is in little endian format.
-///
-/// The first byte is the Head address.
-/// The second byte is split between the upper bits of the Cylinder address and
-/// the Sector address.
-/// The third byte is the lower bits of the Cylinder address.
-///
-/// Access and writing methods are provided for you.
-pub struct CylinderHeadSectorAddress {
-    data: [u8; 3],
-    }
-
-impl CylinderHeadSectorAddress {
-    /// Get the Cylinder out of a packed CHS structure.
-    pub fn get_cylinder(&self) -> u16 {
-        let high_cylinder: u8 = self.data[1];
-        let low_cylinder: u8 = self.data[2];
-        let mut return_value: u16;
-        return_value = ((high_cylinder & CYLINDER_MASK) << 2) as u16
-            + low_cylinder as u16;
-        return return_value;
-    }
-
-    /// Get the Head out of a packed CHS structure.
-    pub fn get_head(&self) -> u8 {
-        return self.data[0];
-    }
-
-    /// Get the Sector out of a pack CHS structure.
-    pub fn get_sector(&self) -> u8 {
-        return self.data[2] & SECTOR_MASK;
-    }
-
-    /// Set the Cylinder in a packed CHS structure.
-    pub fn set_cylinder(&mut self, cylinder: u16) {
-        let high_cylinder: u8;
-        let low_cylinder: u8 = cylinder as u8;
-        high_cylinder = ((cylinder >> 2) as u8 & CYLINDER_MASK) as u8;
-        self.data[1] = high_cylinder + (self.data[1] & SECTOR_MASK);
-        self.data[2] = low_cylinder;
-    }
-
-    /// Set the Head in a packed CHS structure.
-    pub fn set_head(&mut self, head: u8) {
-        self.data[0] = head;
-    }
-
-    /// Set the Sector in a packed CHS structure.
-    pub fn set_sector(&mut self, sector: u8) {
-        self.data[1] = (sector & CYLINDER_MASK) + (self.data[1] & SECTOR_MASK);
+impl PartitionEntry {
+    pub fn empty() -> PartitionEntry {
+        return PartitionEntry{ status: PartitionStatus::NotBootable,
+            chs_first_absolute_sector: CylinderHeadSectorAddress::empty(),
+            partition_type: 0,
+            chs_last_absolute_sector: CylinderHeadSectorAddress::empty(),
+            lba_first_absolute_sector: LogicalBlockAddress::empty(),
+            lba_length: LogicalBlockAddress::empty()
+        };
     }
 }
 
+
 /// LBA or Logical Block Addressing is the replacement for CHS for specifying
 /// the size of a partition. MBR uses a 32 bit little endian integer for this
-/// task.
+/// task. The accessory functions already account for to and from little endian
+/// conversion.
 pub struct LogicalBlockAddress {
     data: u32,
 }
 
 impl LogicalBlockAddress {
+    /// Returns an empty LBA
+    pub fn empty() -> LogicalBlockAddress {
+        return LogicalBlockAddress { data: 0 };
+    }
+    
     pub fn read_from_bytes(bytes:&mut &[u8]) -> Option<LogicalBlockAddress> {
         if bytes.len() < 4 {
             // We don't have enough input data.
@@ -110,8 +101,43 @@ impl LogicalBlockAddress {
         self.data = address;
     }
 
-    /// Convert a `LogicalBlockAddress` to a byte array
+    /// Convert a `LogicalBlockAddress` to a byte array.
     pub fn write_to_bytes(&self) -> [u8; 4] {
        return u32::to_le_bytes(self.data);
     }
+}
+
+#[repr(u8)]
+pub enum PartitionType {
+    /// Designates that the table entry is empty.
+    Empty = 0x00,
+    /// Obsolete as of MS-DOS 3.3
+    DosFat12,
+    /// Obsolete -- Old Unix V7 Port 
+    XenixRoot,
+    /// Obsolete -- Old Unix V7 Port
+    XenixUsr,
+    /// Obsolete -- Sub 32MebiByte partitions from DOS 3.0
+    Dos3Fat16,
+    /// Obsolete -- DOS 3.3+ Extended Partition
+    Dos3Extended,
+    /// DOS 3.31 FAT 16
+    Dos331Fat16,
+    /// NTFS and exFAT use the same id
+    NtfsExfat,
+    /// AIX Boot Partition
+    AixBoot,
+    /// AIX Data Partition
+    AixData,
+    /// OS/2 Boot Manager
+    OS2BootManger,
+    Win95Fat32,
+    /// LBA version of Win95Fat32
+    Win95Fat32LBA,
+    /// Avoid if MSDOS is also used.
+    Win95Fat16LBA = 0x0e,
+    /// Avoid if MSDOS is also used.
+    Win95ExtendedLBA,
+    /// Hidden by OS/2
+    HiddenDosFat12 = 0x11,
 }
